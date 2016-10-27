06a69ed4869acf635878507bdae5110c060ed623
Ariq Nugraha <mochariqnugraha@yahoo.co.id>
2016-10-27 10:56:08 +0700

    Update dashboard-value.php
    dashboard

--- template/index/dashboard-value.php
+++ template/index/dashboard-value.php
@@ -26,16 +26,7 @@
 			<div class="wrap-content container" id="container">
 				<!-- start: BREADCRUMB -->
 				<div class="breadcrumb-wrapper">
-					<h4 class="mainTitle no-margin">Welcome to Packet</h4>
-					<span class="mainDescription">overview &amp; stats </span>
-					<ul class="pull-right breadcrumb">
-						<li>
-							<a href="index.php"><i class="fa fa-home margin-right-5 text-large text-dark"></i>Dashboard</a>
-						</li>
-						<li>
-							Dashboard
-						</li>
-					</ul>
+					<h4 class="mainTitle no-margin">Dashboard</h4>
 				</div>
 				<!-- end: BREADCRUMB -->
 				<div class="container padding-top-30">
